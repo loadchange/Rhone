7ecb3c583a1f8d6221749e11a6a3c33d266285fb
LoadChange <soocto@gmail.com>
2018-07-06 17:55:41 +0800

    test: onion model

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const Koa = require('koa');
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  console.log(1);
+  await next();
+  console.log(2);
+  ctx.body = 'hello koa!   1 3 5 4 2';
+});
+
+app.use(async (ctx, next) => {
+  console.log(3);
+  await next();
+  console.log(4);
+});
+
 app.use(ctx => {
-  ctx.body = 'hello koa2!';
+  console.log(5);
 });
 
 app.listen(3000);
