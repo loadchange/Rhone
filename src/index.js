d416f10db8c32fb1429941b80d06ad67eb58c37f
LoadChange <soocto@gmail.com>
2018-07-09 19:40:57 +0800

    feat: api define

--- src/index.js
+++ src/index.js
@@ -4,40 +4,32 @@ const app = new Koa();
 const router = new Router();
 const userRouter = new Router({ prefix: '/users' });
 
-const auth = async (ctx, next) => {
-  console.log(ctx.url);
-  if (ctx.url !== '/users') {
-    ctx.throw(401);
-  }
-  next();
-};
-
 router.get('/', ctx => {
   ctx.body = 'home!';
 });
 
-userRouter.get('/', auth, ctx => {
-  ctx.body = 'user list';
+userRouter.get('/', ctx => {
+  ctx.body = [{ name: 'Steve jobs' }, { name: 'Obama' }];
 });
 
-userRouter.post('/', auth, ctx => {
-  ctx.body = 'create user';
+userRouter.post('/', ctx => {
+  ctx.body = { name: 'Lincoln' };
 });
 
-userRouter.get('/:id', auth, ctx => {
-  ctx.body = `query user by id = ${ctx.params.id}`;
+userRouter.get('/:id', ctx => {
+  ctx.body = { name: 'Lincoln' };
+});
+
+userRouter.put('/:id', ctx => {
+  ctx.body = { name: 'Lincoln' };
+});
+
+userRouter.delete('/:id', ctx => {
+  ctx.status = 204;
 });
 
 app.use(router.routes());
 app.use(userRouter.routes());
-/**
- * options
- * 检测服务支持的请求方法
- * cors 的预检请求
- * 
- * allowedMethods 用于响应 options 所支持的方法
- * 可以响应的返回 405 不支持 、501 未实现
- */
 app.use(userRouter.allowedMethods());
 
 app.listen(3000);
