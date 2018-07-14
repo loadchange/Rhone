04be5956889ca4b263eabd891cf2053382359b5a
LoadChange <soocto@gmail.com>
2018-07-14 22:10:01 +0800

    test: simulation function

--- src/index.js
+++ src/index.js
@@ -1,40 +1,9 @@
 const Koa = require('koa');
-const Router = require('koa-router');
 const bodyparser = require('koa-bodyparser');
+const routing = require('./routes');
 const app = new Koa();
-const router = new Router();
-const userRouter = new Router({ prefix: '/users' });
 
-router.get('/', ctx => {
-  ctx.body = 'home!';
-});
-
-userRouter.get('/', ctx => {
-  ctx.body = [{ name: 'Steve jobs' }, { name: 'Obama' }];
-});
-
-userRouter.post('/', ctx => {
-  ctx.body = { name: 'Lincoln' };
-});
-
-userRouter.get('/:id', ctx => {
-  ctx.body = { name: 'Lincoln' };
-});
-
-userRouter.put('/:id', ctx => {
-  ctx.body = { name: 'Lincoln' };
-});
-
-userRouter.delete('/:id', ctx => {
-  ctx.status = 204;
-});
-
-/**
- * 引入 koa-bodyparser 解析请求体 body
- */
+routing(app);
 app.use(bodyparser());
-app.use(router.routes());
-app.use(userRouter.routes());
-app.use(userRouter.allowedMethods());
 
-app.listen(3000);
+app.listen(3000, () => console.log('start... \nhttp://localhost:3000'));
