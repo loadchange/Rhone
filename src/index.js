64fac718f806f2eee73bf66e4dadb7fdd90c3c08
LoadChange <soocto@gmail.com>
2018-07-24 18:59:14 +0800

    feat: MongoDB Connection

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const Koa = require('koa');
 const bodyparser = require('koa-bodyparser');
 const error = require('koa-json-error');
 const parameter = require('koa-parameter');
+const mongoose = require('mongoose');
 const routing = require('./routes');
+const { connectionStr } = require('./config');
 const app = new Koa();
+
+mongoose.connect(connectionStr, { useNewUrlParser: true }, () => console.log('MongoDB Connection successful!'));
+mongoose.connection.on('error', console.error);
 
 app.use(bodyparser());
 parameter(app);
