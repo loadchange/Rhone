e7018bf832dfab7d05f6e218de7868d9cce62041
LoadChange <soocto@gmail.com>
2018-08-20 17:49:12 +0800

    feat: install koa-jwt

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,20 +1,10 @@
 const Router = require('koa-router');
-const jsonwebtoken = require('jsonwebtoken');
+const jwt = require('koa-jwt');
 const router = new Router({ prefix: '/users' });
 const { find, findById, create, update, del, login, checkOwner } = require('../controllers/users');
 const { secret } = require('../config.js');
 
-const auth = async (ctx, next) => {
-  const { authorization = '' } = ctx.request.header;
-  const token = authorization.replace('Bearer ', '');
-  try {
-    const user = jsonwebtoken.verify(token, secret);
-    ctx.state.user = user;
-  } catch (err) {
-    ctx.throw(401, err.message);
-  }
-  await next();
-};
+const auth = jwt({ secret });
 
 router.get('/', find);
 router.post('/', create);
