84b67145ebdc530fcd09ed12c548b5a06c2798b2
LoadChange <soocto@gmail.com>
2018-09-10 21:29:31 +0800

    feat: user follow api

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,19 @@
 const Router = require('koa-router');
 const jwt = require('koa-jwt');
 const router = new Router({ prefix: '/users' });
-const { find, findById, create, update, del, login, checkOwner } = require('../controllers/users');
+const {
+  find,
+  findById,
+  create,
+  update,
+  del,
+  login,
+  checkOwner,
+  listFollowers,
+  listFollowing,
+  follow,
+  unfollow,
+} = require('../controllers/users');
 const { secret } = require('../config.js');
 
 const auth = jwt({ secret });
@@ -12,5 +24,10 @@ router.get('/:id', findById);
 router.patch('/:id', auth, checkOwner, update);
 router.delete('/:id', auth, checkOwner, del);
 router.post('/login', login);
+
+router.get('/:id/followers', listFollowers);
+router.get('/:id/following', listFollowing);
+router.put('/following/:id', auth, follow);
+router.delete('/following/:id', auth, unfollow);
 
 module.exports = router;
