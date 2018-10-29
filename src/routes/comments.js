68060ada5b4cbeeeccbc943078806f212e00dac2
LoadChange <soocto@gmail.com>
2018-10-29 23:50:18 +0800

    feat: comment router

---
+++ src/routes/comments.js
@@ -0,0 +1,16 @@
+const jwt = require('koa-jwt');
+const Router = require('koa-router');
+const router = new Router({ prefix: '/questions/:questionId/answers/:answerId/comments' });
+const { find, findById, create, update, del, checkCommentExist, checkCommentator } = require('../controllers/comments');
+
+const { secret } = require('../config');
+
+const auth = jwt({ secret });
+
+router.get('/', find);
+router.post('/', auth, create);
+router.get('/:id', checkCommentExist, findById);
+router.patch('/:id', auth, checkCommentExist, checkCommentator, update);
+router.delete('/:id', auth, checkCommentExist, checkCommentator, del);
+
+module.exports = router;
