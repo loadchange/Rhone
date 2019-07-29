6ff580a238c681b2f30232e3c91f6b8469e81bc7
LoadChange <soocto@gmail.com>
2019-07-29 23:19:04 +0800

    test: post

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -77,3 +77,34 @@ rhone({
   url: '/base/buffer',
   data: arr
 })
+
+rhone({
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+rhone({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json',
+    'Accept': 'application/json, text/plain, */*'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramsString)
+
+rhone({
+  method: 'post',
+  url: '/base/post',
+  data: searchParams
+})
