9d42db9bc8b2e0e8996d29cf466ccd417dcec8a3
LoadChange <soocto@gmail.com>
2019-09-01 12:40:11 +0800

    feat: progress

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -3,6 +3,7 @@ import { parseHeaders } from '../helpers/headers'
 import { createError } from '../helpers/error'
 import { isURLSameOrigin } from '../helpers/url'
 import cookie from '../helpers/cookie'
+import { isFormData } from '../helpers/util'
 
 export default function xhr(config: RhoneRequestConfig): RhonePromise {
   return new Promise((resolve, reject) => {
@@ -16,68 +17,98 @@ export default function xhr(config: RhoneRequestConfig): RhonePromise {
       cancelToken,
       withCredentials,
       xsrfHeaderName,
-      xsrfCookieName
+      xsrfCookieName,
+      onDownloadProgress,
+      onUploadProgress
     } = config
     const request = new XMLHttpRequest()
-    if (responseType) {
-      request.responseType = responseType
-    }
-    if (timeout) {
-      request.timeout = timeout
-    }
-    if (withCredentials) {
-      request.withCredentials = withCredentials
-    }
+
     request.open(method.toUpperCase(), url!, true)
-    request.onreadystatechange = function handleLoad() {
-      if (request.readyState !== 4) {
-        return
+
+    configureRequest()
+
+    addEvents()
+
+    processHeaders()
+
+    processCancel()
+
+    request.send(data)
+
+    function configureRequest(): void {
+      if (responseType) {
+        request.responseType = responseType
       }
-      if (request.status === 0) {
-        return
+      if (timeout) {
+        request.timeout = timeout
       }
-      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
-      const responseData = responseType !== 'text' ? request.response : request.responseText
-      const response: RhoneResponse = {
-        data: responseData,
-        status: request.status,
-        statusText: request.statusText,
-        headers: responseHeaders,
-        config,
-        request
+      if (withCredentials) {
+        request.withCredentials = withCredentials
       }
-      handleResponse(response)
     }
 
-    request.onerror = function handleError() {
-      reject(createError('Network Error', config, null, request))
-    }
+    function addEvents(): void {
+      request.onreadystatechange = function handleLoad() {
+        if (request.readyState !== 4) {
+          return
+        }
+        if (request.status === 0) {
+          return
+        }
+        const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+        const responseData = responseType !== 'text' ? request.response : request.responseText
+        const response: RhoneResponse = {
+          data: responseData,
+          status: request.status,
+          statusText: request.statusText,
+          headers: responseHeaders,
+          config,
+          request
+        }
+        handleResponse(response)
+      }
+      request.onerror = function handleError() {
+        reject(createError('Network Error', config, null, request))
+      }
 
-    request.ontimeout = function handleError() {
-      reject(createError(`Timeout of ${timeout}ms exceeded`, config, 'ECONNABORTED', request))
-    }
-    if ((withCredentials || isURLSameOrigin(url!)) && xsrfCookieName) {
-      const xsrfValue = cookie.read(xsrfCookieName)
-      if (xsrfValue && xsrfHeaderName) {
-        headers[xsrfHeaderName] = xsrfValue
+      request.ontimeout = function handleError() {
+        reject(createError(`Timeout of ${timeout}ms exceeded`, config, 'ECONNABORTED', request))
+      }
+      if (onDownloadProgress) {
+        request.onprogress = onDownloadProgress
+      }
+      if (onUploadProgress) {
+        request.upload.onprogress = onUploadProgress
       }
     }
-    Object.keys(headers).forEach(name => {
-      if (data === null && name.toLowerCase() === 'content-type') {
-        delete headers[name]
-        return
-      }
-      request.setRequestHeader(name, headers[name])
-    })
 
-    if (cancelToken) {
-      cancelToken.promise.then(reason => {
-        request.abort()
-        reject(reason)
+    function processHeaders(): void {
+      if (isFormData(data)) {
+        delete headers['Content-Type']
+      }
+      if ((withCredentials || isURLSameOrigin(url!)) && xsrfCookieName) {
+        const xsrfValue = cookie.read(xsrfCookieName)
+        if (xsrfValue && xsrfHeaderName) {
+          headers[xsrfHeaderName] = xsrfValue
+        }
+      }
+      Object.keys(headers).forEach(name => {
+        if (data === null && name.toLowerCase() === 'content-type') {
+          delete headers[name]
+          return
+        }
+        request.setRequestHeader(name, headers[name])
       })
     }
 
-    request.send(data)
+    function processCancel(): void {
+      if (cancelToken) {
+        cancelToken.promise.then(reason => {
+          request.abort()
+          reject(reason)
+        })
+      }
+    }
 
     function handleResponse(response: RhoneResponse): void {
       if (response.status >= 200 && response.status < 300) {
