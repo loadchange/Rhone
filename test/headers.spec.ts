bddb219c8f9dcb9144b4461846ffa1651f760bdd
LoadChange <soocto@gmail.com>
2019-09-18 18:59:12 +0800

    test: headers spec

---
+++ test/headers.spec.ts
@@ -0,0 +1,87 @@
+import rhone, { RhoneError, RhoneResponse } from '../src'
+import { getAjaxRequest } from './helper'
+
+function testHeaderValue(headers: any, key: string, val?: string) {
+  let found = false
+  for (const k in headers) {
+    found = k.toLowerCase() === key.toLowerCase()
+    if (found) {
+      expect(headers[k]).toBe(val)
+      break
+    }
+  }
+
+  if (!found) {
+    if (typeof val === 'undefined') {
+      expect(headers.hasOwnProperty(key)).toBeFalsy()
+    } else {
+      throw new Error(`${key} was not found in headers`)
+    }
+  }
+}
+
+describe('requests', () => {
+  beforeEach(() => {
+    jasmine.Ajax.install()
+  })
+  afterEach(() => {
+    jasmine.Ajax.uninstall()
+  })
+
+  test('should use default common headers', () => {
+    const headers = rhone.defaults.headers.common
+
+    rhone('/foo')
+
+    return getAjaxRequest().then(request => {
+      for (const key in headers) {
+        if (headers.hasOwnProperty(key)) {
+          expect(request.requestHeaders[key]).toEqual(headers[key])
+        }
+      }
+    })
+  })
+
+  test('should add extra headers for post', () => {
+    rhone.post('/foo', 'fizz=buzz')
+
+    return getAjaxRequest().then(request => {
+      testHeaderValue(request.requestHeaders, 'Content-Type', 'application/x-www-form-urlencoded')
+    })
+  })
+
+  test('should use application/json when posting an object', () => {
+    rhone.post('/foo/bar', { firstName: 'foo', lastName: 'bar' })
+
+    return getAjaxRequest().then(request => {
+      testHeaderValue(request.requestHeaders, 'Content-Type', 'application/json;charset=utf-8')
+    })
+  })
+
+  test('should remove content-type if data is empty', () => {
+    rhone.post('/foo/bar')
+
+    return getAjaxRequest().then(request => {
+      testHeaderValue(request.requestHeaders, 'Content-Type', undefined)
+    })
+  })
+
+  test('should preserve content-type if data is false', () => {
+    rhone.post('/foo/bar', false)
+
+    return getAjaxRequest().then(request => {
+      testHeaderValue(request.requestHeaders, 'Content-Type', 'application/x-www-form-urlencoded')
+    })
+  })
+
+  test('should remove content-type if data is FormData', () => {
+    const data = new FormData()
+    data.append('foo', 'bar')
+
+    rhone.post('/foo', data)
+
+    return getAjaxRequest().then(request => {
+      testHeaderValue(request.requestHeaders, 'Content-Type', undefined)
+    })
+  })
+})
