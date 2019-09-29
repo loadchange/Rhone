15ac7a5e0338bdf22d698f18678093e1e2d67e4d
LoadChange <soocto@gmail.com>
2019-09-29 21:47:18 +0800

    test: transform spec

---
+++ test/transform.spec.ts
@@ -0,0 +1,94 @@
+import rhone, { RhoneResponse, RhoneTransformer } from '../src/index'
+import { getAjaxRequest } from './helper'
+
+describe('transform', () => {
+  beforeEach(() => {
+    jasmine.Ajax.install()
+  })
+
+  afterEach(() => {
+    jasmine.Ajax.uninstall()
+  })
+
+  test('should transform JSON to string', () => {
+    const data = {
+      foo: 'bar'
+    }
+
+    rhone.post('/foo', data)
+
+    return getAjaxRequest().then(request => {
+      expect(request.params).toBe('{"foo":"bar"}')
+    })
+  })
+
+  test('should transform string to JSON', done => {
+    let response: RhoneResponse
+
+    rhone('/foo').then(res => {
+      response = res
+    })
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200,
+        responseText: '{"foo": "bar"}'
+      })
+
+      setTimeout(() => {
+        expect(typeof response.data).toBe('object')
+        expect(response.data.foo).toBe('bar')
+        done()
+      }, 100)
+    })
+  })
+
+  test('should override default transform', () => {
+    const data = {
+      foo: 'bar'
+    }
+
+    rhone.post('/foo', data, {
+      transformRequest(data) {
+        return data
+      }
+    })
+
+    return getAjaxRequest().then(request => {
+      expect(request.params).toEqual({ foo: 'bar' })
+    })
+  })
+
+  test('should allow an Array of transformers', () => {
+    const data = {
+      foo: 'bar'
+    }
+
+    rhone.post('/foo', data, {
+      transformRequest: (rhone.defaults.transformRequest as RhoneTransformer[]).concat(function(
+        data
+      ) {
+        return data.replace('bar', 'baz')
+      })
+    })
+
+    return getAjaxRequest().then(request => {
+      expect(request.params).toBe('{"foo":"baz"}')
+    })
+  })
+
+  test('should allowing mutating headers', () => {
+    const token = Math.floor(Math.random() * Math.pow(2, 64)).toString(36)
+
+    rhone('/foo', {
+      transformRequest: (data, headers) => {
+        headers['X-Authorization'] = token
+        return data
+      }
+    })
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders['X-Authorization']).toEqual(token)
+    })
+  })
+})
